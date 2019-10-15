72eb756b7391b24d1351333368ebcffc03a38ae8
Roman Orac <orac.roman@gmail.com>
2019-10-15 20:52:49 +0200

    Comment unsupported code

--- tf_core/nltoolkit/lib/document_converters.py
+++ tf_core/nltoolkit/lib/document_converters.py
@@ -1,60 +1,61 @@
 
-import re
-import os.path
-# from docx import opendocx, getdocumenttext #TODO update with python-docx
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from io import StringIO
-from subprocess import Popen, PIPE
+# TODO Rewrite this part, because pdfminer doesn't work under python 3.6
+# import re
+# import os.path
+# # from docx import opendocx, getdocumenttext #TODO update with python-docx
+# from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+# from pdfminer.converter import TextConverter
+# from pdfminer.layout import LAParams
+# from pdfminer.pdfpage import PDFPage
+# from io import StringIO
+# from subprocess import Popen, PIPE
 
 
-def convert_pdf_to_txt(path):
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    fp = file(path, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos = set()
+# def convert_pdf_to_txt(path):
+#     rsrcmgr = PDFResourceManager()
+#     retstr = StringIO()
+#     codec = 'utf-8'
+#     laparams = LAParams()
+#     device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+#     fp = file(path, 'rb')
+#     interpreter = PDFPageInterpreter(rsrcmgr, device)
+#     password = ""
+#     maxpages = 0
+#     caching = True
+#     pagenos = set()
 
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=True):
-        interpreter.process_page(page)
-    fp.close()
-    device.close()
-    str = retstr.getvalue()
-    retstr.close()
-    return str
+#     for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=True):
+#         interpreter.process_page(page)
+#     fp.close()
+#     device.close()
+#     str = retstr.getvalue()
+#     retstr.close()
+#     return str
 
 
-def document_to_text(path):
-    ext = os.path.splitext(path)[1]
-    filename = os.path.basename(path)
+# def document_to_text(path):
+#     ext = os.path.splitext(path)[1]
+#     filename = os.path.basename(path)
 
-    if ext == ".doc":
-        cmd = os.path.dirname(os.path.realpath(__file__)) + os.sep + "antiword" + os.sep + "antiword.exe -m CP852 " + path
-        pipe = Popen(cmd, stdout=PIPE, shell=True)
-        text = pipe.communicate()[0]
-        return filename, str(re.sub("\r|\n", "", text).strip())
+#     if ext == ".doc":
+#         cmd = os.path.dirname(os.path.realpath(__file__)) + os.sep + "antiword" + os.sep + "antiword.exe -m CP852 " + path
+#         pipe = Popen(cmd, stdout=PIPE, shell=True)
+#         text = pipe.communicate()[0]
+#         return filename, str(re.sub("\r|\n", "", text).strip())
 
-    elif ext == ".docx":
-        document = opendocx(path)
-        paratextlist = getdocumenttext(document)
-        newparatextlist = []
-        for paratext in paratextlist:
-            newparatextlist.append(paratext)
-        return filename, "".join(newparatextlist)
+#     elif ext == ".docx":
+#         document = opendocx(path)
+#         paratextlist = getdocumenttext(document)
+#         newparatextlist = []
+#         for paratext in paratextlist:
+#             newparatextlist.append(paratext)
+#         return filename, "".join(newparatextlist)
 
-    elif ext == ".pdf":
-        text = convert_pdf_to_txt(path)
-        return filename, str(re.sub("\r|\n", "", text).strip())
+#     elif ext == ".pdf":
+#         text = convert_pdf_to_txt(path)
+#         return filename, str(re.sub("\r|\n", "", text).strip())
 
-    else:
-        text = open(path, "r").read().strip()
-        return filename, text
+#     else:
+#         text = open(path, "r").read().strip()
+#         return filename, text
 
