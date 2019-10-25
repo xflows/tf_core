5ed1ea3db252b4c37442d92fd44451ece4f4a337
Roman Orac <orac.roman@gmail.com>
2019-10-25 21:14:57 +0200

    Uncomment commented code

--- tf_core/nltoolkit/lib/document_converters.py
+++ tf_core/nltoolkit/lib/document_converters.py
@@ -1,16 +1,14 @@
-
-# TODO Rewrite this part, because pdfminer doesn't work under python 3.6
-# import re
-# import os.path
-# # from docx import opendocx, getdocumenttext #TODO update with python-docx
+import os.path
+import re
+# from docx import opendocx, getdocumenttext # TODO update with python-docx
 # from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 # from pdfminer.converter import TextConverter
 # from pdfminer.layout import LAParams
 # from pdfminer.pdfpage import PDFPage
 # from io import StringIO
-# from subprocess import Popen, PIPE
+from subprocess import PIPE, Popen
 
-
+# TODO Rewrite this part, pdfminer doesn't work under python 3
 # def convert_pdf_to_txt(path):
 #     rsrcmgr = PDFResourceManager()
 #     retstr = StringIO()
@@ -33,29 +31,33 @@
 #     return str
 
 
-# def document_to_text(path):
-#     ext = os.path.splitext(path)[1]
-#     filename = os.path.basename(path)
+def convert_pdf_to_txt(path):
+    raise Exception("This part of the code is not yet ported to Python3")
 
-#     if ext == ".doc":
-#         cmd = os.path.dirname(os.path.realpath(__file__)) + os.sep + "antiword" + os.sep + "antiword.exe -m CP852 " + path
-#         pipe = Popen(cmd, stdout=PIPE, shell=True)
-#         text = pipe.communicate()[0]
-#         return filename, str(re.sub("\r|\n", "", text).strip())
 
-#     elif ext == ".docx":
-#         document = opendocx(path)
-#         paratextlist = getdocumenttext(document)
-#         newparatextlist = []
-#         for paratext in paratextlist:
-#             newparatextlist.append(paratext)
-#         return filename, "".join(newparatextlist)
+def document_to_text(path):
+    ext = os.path.splitext(path)[1]
+    filename = os.path.basename(path)
 
-#     elif ext == ".pdf":
-#         text = convert_pdf_to_txt(path)
-#         return filename, str(re.sub("\r|\n", "", text).strip())
+    if ext == ".doc":
+        cmd = os.path.dirname(os.path.realpath(
+            __file__)) + os.sep + "antiword" + os.sep + "antiword.exe -m CP852 " + path
+        pipe = Popen(cmd, stdout=PIPE, shell=True)
+        text = pipe.communicate()[0]
+        return filename, str(re.sub("\r|\n", "", text).strip())
 
-#     else:
-#         text = open(path, "r").read().strip()
-#         return filename, text
+    elif ext == ".docx":
+        document = opendocx(path)
+        paratextlist = getdocumenttext(document)
+        newparatextlist = []
+        for paratext in paratextlist:
+            newparatextlist.append(paratext)
+        return filename, "".join(newparatextlist)
 
+    elif ext == ".pdf":
+        text = convert_pdf_to_txt(path)
+        return filename, str(re.sub("\r|\n", "", text).strip())
+
+    else:
+        text = open(path, "r").read().strip()
+        return filename, text
